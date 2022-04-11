b6dd2726d3287039c41880ec527f502e64bcf061
chapa0723 <dchaparro0723@gmail.com>
2022-04-10 23:26:54 -0300

    Swagger issue&Mongoose schema user data

---
+++ src/domain/orm/User.oem.ts
@@ -0,0 +1,27 @@
+import { userEntity } from '..entities/User.entity';
+import { LogSuccess, LogError } from '@/utils/logger';
+
+// CRUD
+/**
+ * Method to optain all user from collection  "Users" in Mongo Server
+ */
+export const GetAllUsers = async () => {
+  try {
+    let userModel = userEntity();
+
+    // Search All User
+    return await userModel.find({isDelete: false});
+
+  } catch (error) {
+    LogError(`[ORM ERROR]: Getting ALl Users: ${error}`);
+    
+  }
+}
+
+
+// TODO
+// Get User by ID
+// Ger User by Email
+// Delete User by ID
+// Create New User
+// Update User by ID
